18650411f463e3a4b8b261fe1998b780aa05eb2e
cynthiasants <cynthiasants24@gmail.com>
2020-01-10 08:53:33 +0700

    Update APegawai.java

--- src/main/APegawai.java
+++ src/main/APegawai.java
@@ -5,13 +5,18 @@
  */
 package main;
 
+import crude.CBarang;
+import crude.CKategori;
+import crude.CPegawai;
+import java.util.Scanner;
+
 /**
  *
  * @author Cynthia
  */
 public class APegawai extends Admin {
     public APegawai(int idp){
-        setAdmin(1);
+        super(idp);
     }
     public void banner(){
         System.out.println("=================================================");
@@ -19,5 +24,53 @@ public class APegawai extends Admin {
         System.out.println("|                Admin - Pegawai                |");
         System.out.println("=================================================");
     }
-    
+     public void start(){
+        banner();
+        while (true){
+            System.out.println("[1] Lihat Pegawai");
+            System.out.println("[2] Tambah Pegawai");
+            System.out.println("[3] Edit Pegawai");
+            System.out.println("[4] Hapus Pegawai");
+            System.out.print("[$] ");
+            CPegawai pe = new CPegawai();
+            Scanner inp = new Scanner(System.in);
+            int p = inp.nextInt();
+            if (p == 1){
+                pe.select(getConnection());
+            }
+            else if (p == 2 || p == 3){
+                int id = 0;
+                if (p == 3){
+                    pe.select(getConnection());
+                    System.out.print("Masukkan ID : ");
+                    id = inp.nextInt();
+                    inp.nextLine();
+                }
+
+                CKategori k = new CKategori();
+                k.select(getConnection());
+                System.out.print("Masukkan Kode Barang : ");
+                int idk = inp.nextInt();
+                System.out.print("Nama Barang\t: ");
+                String nama = inp.nextLine();
+                System.out.print("Harga Barang\t: ");
+                int harga = inp.nextInt();
+                System.out.print("Stok Barang\t: ");
+                int stok = inp.nextInt();
+                if (p == 3){
+                    pe.update(getConnection(), id, nama, harga, stok);
+                }
+                else {
+                    pe.insert(getConnection(), idk, nama, harga, stok);
+                }
+            }
+            else if(p == 4){
+                pe.select(getConnection());
+                System.out.print("Masukkan ID : ");
+                int id = inp.nextInt();
+                inp.nextLine();
+                pe.delete(getConnection(),id);
+            }
+        }
+    }
 }
